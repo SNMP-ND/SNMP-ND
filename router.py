14892000ea21711f64a53137b164d8d44cff5500
peremunoz <peremunozfiguerol33@gmail.com>
2023-05-24 19:23:27 +0200

    Implemented class

--- router.py
+++ router.py
@@ -1,5 +1,23 @@
 # Class representing a Router
 
-class Router():
-    sysName = ""
+from interface import Interface
+
+class Router:
+    sysName : str
+    ospfNeighborsIPs : list
+    interfaces : list
+
+    def __init__(self, sysName):
+        self.sysName = sysName
+        self.ospfNeighborsIPs = []
+        self.interfaces = []
+    
+    def __str__(self):
+        return f'Router {self.sysName}: Neighbors: {self.ospfNeighborsIPs}, interfaces: {self.interfaces}'
+
+    def setNeighbors(self, neighbors : list):
+        self.ospfNeighborsIPs = neighbors
+    
+    def setInterfaces(self, interface : list):
+        self.interfaces = interface
     
