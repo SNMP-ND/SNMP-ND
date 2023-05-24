9e1d36240fbed6e65be8464435f4329ab6878fd7
peremunoz <peremunozfiguerol33@gmail.com>
2023-05-24 19:23:18 +0200

    Implemented class

--- interface.py
+++ interface.py
@@ -1,5 +1,19 @@
+# Class representing an Interface
+
 class Interface:
-    index = ''
-    status = ''
-    speed = ''
-    ip = ''
+    index : int
+    description : str
+    status : str
+    speed : int
+    ip : str
+
+    def __init__(self, index, description, status, speed, ip):
+        self.index = index
+        self.description = description
+        self.status = status
+        self.speed = speed
+        self.ip = ip
+
+    def __str__(self):
+        return f'Interface {self.index}: {self.description}, {self.status}, {self.speed}, {self.ip}'
+    
