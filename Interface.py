bec1f0c5c20331f7041b2fa8f4de28f33017a044
peremunoz <peremunozfiguerol33@gmail.com>
2023-06-02 19:50:30 +0200

    Add mask attribute

--- Interface.py
+++ Interface.py
@@ -6,16 +6,18 @@ class Interface:
     status : str
     speed : int
     ip : str
+    mask : str
 
-    def __init__(self, index, description, status, speed, ip):
+    def __init__(self, index, description, status, speed, ip, mask):
         self.index = index
         self.description = description
         self.status = status
         self.speed = speed
         self.ip = ip
+        self.mask = mask
 
     def __str__(self):
-        return f'Interface {self.index}: {self.description}, {self.status}, {self.speed}, {self.ip}'
+        return f'Interface {self.index}: {self.description}, {self.status}, {self.speed}, {self.ip}, {self.mask}'
     
     # Getters
     def getIndex(self):
