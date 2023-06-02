9c202848726bbcff09b737d1ce286e21433ec859
peremunoz <peremunozfiguerol33@gmail.com>
2023-06-02 20:10:39 +0200

    Add method for checking if an interface is connected to another

--- Interface.py
+++ Interface.py
@@ -35,3 +35,25 @@ class Interface:
     def getIP(self):
         return self.ip
     
+    def getMask(self):
+        return self.mask
+    
+    def isLinkedWith(self, interface: 'Interface') -> bool:
+        # First check if the interfaces are up
+        if (self.status != 'up' or interface.getStatus() != 'up'):
+            return False
+        # Then check if they are in the same subnet
+        if (self.mask != interface.getMask()):
+            return False
+        # Finally, check if they are in the same network depending on the mask
+        ip1 = self.ip.split('.')
+        ip2 = interface.getIP().split('.')
+        # Check if the IPs are valid
+        if (len(ip1) != 4 or len(ip2) != 4):
+            return False
+        mask = self.mask.split('.')
+        for i in range(0, 4):
+            if (int(ip1[i]) & int(mask[i]) != int(ip2[i]) & int(mask[i])):
+                return False
+        return True
+    
