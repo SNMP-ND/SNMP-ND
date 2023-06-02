b428554b2969796cbd7c5015efd55cf27c831f7f
peremunoz <peremunozfiguerol33@gmail.com>
2023-06-02 20:51:17 +0200

    Juicy drawing!

--- TopologyPlotter.py
+++ TopologyPlotter.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
+import math
 
 class TopologyPlotter:
     routers: list
@@ -24,20 +25,30 @@ class TopologyPlotter:
                     for interface2 in router2Interfaces:
 
                         if (interface1.isLinkedWith(interface2)):
-                            label = interface1.getIP() + "-" + interface2.getIP() + " " + str(int(min(interface1.getSpeed(), interface2.getSpeed()))/1000) + "Mbps"
+                            label = interface1.getIP() + "-" + interface2.getIP() + " " + str(int(min(interface1.getSpeed(), interface2.getSpeed()))/1000000) + "Gbps"
                             G.add_edge(router.getSysName(), neighbor.getSysName(), label=label)
                             break
                 
         pos = nx.spring_layout(G)
         
-        plt.figure(figsize=(10, 6))
-        nx.draw_networkx(G, pos, with_labels=True, node_size=500, node_color="lightblue", font_size=10, font_weight="bold", edge_color="gray")
+        plt.figure(figsize=(12, 8))
+        node_size = 1000
+        node_color = "darkblue"
+        font_color = "white"
+        edge_color = "gray"
+        node_shape = "s"  # Use square shape for nodes
+        node_style = nx.draw_networkx_nodes(G, pos, node_size=node_size, node_color=node_color, edgecolors="black", linewidths=1, node_shape=node_shape)
+        node_style.set_edgecolor("black")
+
+        edge_style = nx.draw_networkx_edges(G, pos, edge_color=edge_color, width=1.5, alpha=0.7)
+
+        nx.draw_networkx_labels(G, pos, font_color=font_color, font_size=10, font_weight="bold")
 
         edge_labels = nx.get_edge_attributes(G, 'label')
         for edge, label in edge_labels.items():
             x = (pos[edge[0]][0] + pos[edge[1]][0]) / 2  # Calculate x-coordinate for label position
             y = (pos[edge[0]][1] + pos[edge[1]][1]) / 2 + 0.1  # Calculate y-coordinate for label position
-            plt.text(x, y, label, horizontalalignment='center', verticalalignment='center')
+            plt.text(x, y, label, horizontalalignment='center', verticalalignment='center', fontsize=8)
 
         plt.axis("off")
         plt.show()
