8c501976ea184316fc127dad722850f01fd8832b
Faysal <faysalbadaoui02@gmail.com>
2023-06-18 19:51:13 +0200

    logs written on file in /var/log/snmptraps.log

--- traps/trap_parser.sh
+++ traps/trap_parser.sh
@@ -6,12 +6,12 @@ while read line; do
     second_part=$(echo "$line" | cut -d ' ' -f 2)
 
     if [ "$first_part" = "iso.3.6.1.2.1.14.1.1" ]; then
-        echo " ------------------------------------------------- "
-        echo "| Trap from $second_part received                     |"
+        echo " ------------------------------------------------- ">> /var/log/snmptraps.log
+        echo "| Trap from $second_part received                     |" >> /var/log/snmptraps.log
     fi
 
     if [ "$first_part" = "iso.3.6.1.2.1.14.10.1.1" ]; then
-        echo "| OSPF neighbor with ip $second_part has changed state |"
-        echo " ------------------------------------------------- "
+        echo "| OSPF neighbor with ip $second_part has changed state |" >> /var/log/snmptraps.log
+        echo " ------------------------------------------------- " >> /var/log/snmptraps.log
     fi
 done
